113310b65b14c311f3f6835eb0f5f745fbee2218
Brandon Martinez <Brandon Martinez>
2015-01-30 01:39:32 -0500

    Passed all tests for sentencify.js

--- hw1/src/follow-the-ball/BallFollower.js
+++ hw1/src/follow-the-ball/BallFollower.js
@@ -3,6 +3,19 @@
 
   var BallFollower = function BallFollower(start, swaps) {
     // IMPLEMENT YOUR SOLUTION WITHIN THIS FUNCTION
+    var ballPosition = start;
+    var swapList = swaps;
+    function swap(swapList) {
+        var index;
+        for (index = 0; index < swapList.length; ++index) {
+            if (swapList[index][0] === ballPosition) {
+                ballPosition = swapList[index][1];
+            }else if (swapList[index][1] === ballPosition) {
+                ballPosition = swapList[index][0];
+            }
+        }return ballPosition;
+    };
+    return ballPosition;
   };
 
   module.exports = BallFollower; // DON'T ALTER THIS
--- hw1/src/sentencify/sentencify.js
+++ hw1/src/sentencify/sentencify.js
@@ -3,6 +3,16 @@
 
   var sentencify = function (words) {
     // IMPLEMENT YOUR SOLUTION WITHIN THIS FUNCTION
+    var index;
+    var sentence = '';
+    if (words.length === 0) {
+	return '';
+    }
+    for (index = 0; index < (words.length - 1); ++index) {
+        sentence = sentence + words[index] + ' ';
+    }
+    sentence = sentence + words[words.length - 1];
+    return sentence;
   };
 
   module.exports = sentencify;  // DO NOT CHANGE THIS
