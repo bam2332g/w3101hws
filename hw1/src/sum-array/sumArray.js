0205fc487d254bc4296c979cc62db56b2e515a6b
Brandon Martinez <Brandon Martinez>
2015-01-30 01:50:29 -0500

    sumArray.js passed all tests

--- hw1/src/sum-array/sumArray.js
+++ hw1/src/sum-array/sumArray.js
@@ -4,6 +4,8 @@
   var sumArray = function sumArray(values) {
     // IMPLEMENT YOUR SOLUTION HERE!
     // MAKE SURE TO USE REDUCE!
+    return values.reduce(function(a, b) {
+        return a + b; }, 0);
   };
 
   module.exports = sumArray; // DON'T CHANGE THIS
